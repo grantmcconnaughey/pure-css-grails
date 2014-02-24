6ef4fd92347f41ae4baf6a67576fc54fb34851f8
grantmcconnaughey <grantmcconnaughey@gmail.com>
2014-02-23 21:30:22 -0600

    Update _Install.groovy

--- scripts/_Install.groovy
+++ scripts/_Install.groovy
@@ -6,5 +6,4 @@ println '''
 	* Run "grails copy-pure-css" to copy the CSS files over to your application
 	* Use the Resources plugin to add the CSS to your GSPs ( <r:require module="pure-all" /> )
 
-'''
-//	* Use the pure taglib to render style tags for the files ( <pure:pure /> == <style href="path/to/css/pure.css" /> )  
+'''  
