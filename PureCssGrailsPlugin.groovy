dd020c04e4bc19407879cb1ac462d445e9788b9e
Grant McConnaughey <grantmcconnaughey@gmail.com>
2015-03-26 23:30:03 -0500

    Set version 0.6.0

--- PureCssGrailsPlugin.groovy
+++ PureCssGrailsPlugin.groovy
@@ -1,14 +1,14 @@
 class PureCssGrailsPlugin {
-    def version = "0.4.2"
+    def version = "0.6.0"
     def grailsVersion = "2.0 > *"
 
     def title = "Pure CSS Plugin"
     def author = "Grant McConnaughey"
     def authorEmail = "grantmcconnaughey@gmail.com"
-    def description = 'Provides Pure CSS resources to a Grails application.'
+    def description = "Provides Pure CSS resources to a Grails application."
 
     def documentation = "http://grails.org/plugin/pure-css"
     def license = "APACHE"
-    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/PURECSS" ]
+    def issueManagement = [ system: "Github", url: "https://github.com/grantmcconnaughey/pure-css-grails/issues" ]
     def scm = [ url: "https://github.com/grantmcconnaughey/pure-css-grails" ]
 }
